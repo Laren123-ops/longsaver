6838d0ea4729c43d1835db8d24fd05fd29f87f4d
Laren123-ops <2952188097@qq.com>
2026-01-31 23:08:19 +0800

    Update main.jsx

--- app/nocode/src/main.jsx
+++ app/nocode/src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import AuthCallback from "./AuthCallback.jsx"
-
-const isCallback = window.location.pathname === "/auth/callback"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>{isCallback ? <AuthCallback /> : <App />}</React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
