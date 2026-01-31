980e7879c5724385fcc77092905de50e0cbf9e5d
Laren123-ops <2952188097@qq.com>
2026-01-31 19:19:20 +0800

    Simplify rendering by removing AuthCallback
    Removed AuthCallback component and simplified rendering logic.

--- app/nocode/src/main.jsx
+++ app/nocode/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.jsx"
-import AuthCallback from "./AuthCallback.jsx"
-
-const isCallback = window.location.pathname === "/auth/callback"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {isCallback ? <AuthCallback /> : <App />}
+    <App />
   </React.StrictMode>
 )
