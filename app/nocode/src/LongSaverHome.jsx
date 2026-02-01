fa335530cef168be769d4f3137354124c58f757e
Laren123-ops <2952188097@qq.com>
2026-02-01 18:00:24 +0800

    Update LongSaverHome.jsx

--- app/nocode/src/LongSaverHome.jsx
+++ app/nocode/src/LongSaverHome.jsx
@@ -5,7 +5,30 @@ export default function LongSaverHome({ user }) {
       <h1>LongSaver</h1>
       <p>已登录：{user?.email}</p>
 
-      {/* 这里粘贴/渲染你原来做好的页面内容 */}
+      {/import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { navItems } from "./nav-items";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <HashRouter>
+        <Routes>
+          {navItems.map(({ to, page }) => (
+            <Route key={to} path={to} element={page} />
+          ))}
+        </Routes>
+      </HashRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;/}
     </div>
   )
 }
